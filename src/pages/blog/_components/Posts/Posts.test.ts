b22c8664e49498ae774b4234ce618231da281a2b
Mario Pinto <mario.pinto.miranda@gmail.com>
2024-05-04 11:43:25 +0100

    :white_check_mark: <Posts/>
    Took 3 minutes

---
+++ src/pages/blog/_components/Posts/Posts.test.ts
@@ -0,0 +1,36 @@
+import {render} from "@testing-library/react";
+
+import {describe, it} from "vitest";
+
+import Posts from "./Posts.tsx";
+
+
+describe("<Posts/>", () => {
+  it("should not render posts when post are not passed", () => {
+    const result = render(Posts({posts: []}));
+
+    result.getByText("No se encontraron posts");
+  });
+
+  it("should render posts when posts are passed", () => {
+    const posts = [
+      {
+        title: "Post 1",
+        tags: ["tag1", "tag2"],
+        image: {
+          url: "url",
+          alt: "alt"
+        },
+        publicationDate: "date",
+        url: "url"
+      }
+    ];
+
+    const result = render(Posts({posts}));
+
+    result.getByAltText("alt");
+    result.getByText("Post 1");
+    result.getByText("tag1");
+    result.getByText("tag2");
+  });
+})

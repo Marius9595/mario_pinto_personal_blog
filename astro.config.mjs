c48b5aa31be90db9fdca4b9e795b9e5900de13d5
Mario Pinto <mario.pinto.miranda@gmail.com>
2024-05-04 01:50:36 +0100

    :sparkles: Finish MVP
    Took 1 hour 39 minutes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 
 import react from "@astrojs/react";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
-  integrations: [react()]
+  site: "https://mario-pinto-miranda.dev",
+  integrations: [react()],
+  output: 'server',
+  adapter: vercel(),
 });

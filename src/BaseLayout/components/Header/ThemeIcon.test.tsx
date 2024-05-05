87bdbf970f6450cbdf1cca6f2596d58369032fc3
Mario Pinto <mario.pinto.miranda@gmail.com>
2024-05-05 11:46:55 +0100

    :recycle: Change ThemeIcon from astro to react component
    Took 56 minutes

---
+++ src/BaseLayout/components/Header/ThemeIcon.test.tsx
@@ -0,0 +1,59 @@
+import {describe, expect, it, vi} from "vitest";
+import ThemeIcon from "./ThemeIcon.tsx";
+import {fireEvent, render, type RenderResult, waitFor} from "@testing-library/react";
+
+describe('<ThemeIcon/>', () => {
+  const themeRepositoryMock: Storage = {
+    length: 1,
+    clear: vi.fn(),
+    key: vi.fn(),
+    removeItem: vi.fn(),
+    getItem: vi.fn(),
+    setItem: vi.fn()
+  }
+
+  it('should render theme icon with correct theme', () => {
+    themeRepositoryMock.getItem = vi.fn().mockReturnValue('light');
+
+    const themeICon = render(<ThemeIcon themeRepository={themeRepositoryMock}/>)
+
+    themeICon.getByText('🌞');
+    expect(themeRepositoryMock.getItem).toHaveBeenCalled();
+  })
+
+  it('should change light to dark when it is clicked', () => {
+    themeRepositoryMock.getItem = vi.fn().mockReturnValue('light');
+    const themeICon = render(<ThemeIcon themeRepository={themeRepositoryMock}/>)
+
+    fireEvent.click(themeICon.getByText('🌞'))
+
+    assertHasChangedToLightThemeAfterClickOn(themeICon)
+  })
+
+  it('should change dark to light when it is clicked', () => {
+    themeRepositoryMock.getItem = vi.fn().mockReturnValue('dark');
+    const themeICon = render(<ThemeIcon themeRepository={themeRepositoryMock}/>)
+
+    fireEvent.click(themeICon.getByText('🌜'))
+
+    assertHasChangedToDarkThemeAfterClickOn(themeICon)
+  })
+
+  function assertHasChangedToLightThemeAfterClickOn(themeICon: RenderResult) {
+    themeICon.getByText('🌞');
+    expect(document.documentElement.classList).toContain('light');
+    expect(document.documentElement.classList).not.toContain('dark');
+    expect(themeRepositoryMock.setItem).toHaveBeenCalledWith('theme', 'light');
+    expect(themeRepositoryMock.getItem).toHaveBeenCalledWith('theme');
+  }
+
+  function assertHasChangedToDarkThemeAfterClickOn(themeICon: RenderResult) {
+    themeICon.getByText('🌜');
+    expect(document.documentElement.classList).toContain('dark');
+    expect(document.documentElement.classList).not.toContain('light');
+    expect(themeRepositoryMock.setItem).toHaveBeenCalledWith('theme', 'dark');
+    expect(themeRepositoryMock.getItem).toHaveBeenCalledWith('theme');
+  }
+
+  // It is no possible to test styles due to style components. More info here: https://github.com/testing-library/jest-dom/issues/295
+})
---
+++ src/BaseLayout/components/Header/ThemeIcon.tsx
@@ -0,0 +1,25 @@
+import {useState} from "react";
+
+import s from "./ThemeIcon.module.css";
+
+type Theme = "light" | "dark";
+export default function ThemeIcon(
+    {themeRepository = localStorage}: {themeRepository:Storage}
+) {
+
+  const [theme, setTheme] = useState(themeRepository.getItem('theme') as Theme || 'light');
+  const [icon, setIcon] = useState(theme === 'light' ? '🌞' : '🌜');
+
+  const handleClick = () => {
+    document.documentElement.classList.remove(theme);
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+    themeRepository.setItem('theme', newTheme)
+    document.documentElement.classList.add(newTheme);
+    setIcon(newTheme === 'light' ? '🌞' : '🌜');
+  }
+
+  return (
+      <button id="themeToggle" className={s.themeToggle} onClick={handleClick}>{icon}</button>
+  )
+}
